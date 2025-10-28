27f4e5b84a33238b68d1abe99894b61e7a7066d8
Ghenadie Isacenco <isacenco@gmail.com>
2025-10-28 17:42:17 +0100

    Created Tab View

---
+++ Avocados/View/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Avocados
+//
+//  Created by Ghenadie Isacenco on 28/10/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
