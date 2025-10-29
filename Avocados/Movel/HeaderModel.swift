99d7b444d45200561a93ee07308588a3448db477
Ghenadie Isacenco <isacenco@gmail.com>
2025-10-29 13:52:46 +0100

    Header Data

---
+++ Avocados/Movel/HeaderModel.swift
@@ -0,0 +1,16 @@
+//
+//  HeaderModel.swift
+//  Avocados
+//
+//  Created by Ghenadie Isacenco on 29/10/2025.
+//
+
+import SwiftUI
+
+struct Header: Identifiable {
+    var id = UUID()
+    var image: String
+    var headline: String
+    var subheadline: String
+}
+
