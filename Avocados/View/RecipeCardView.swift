184b85fac56faf4d9fa2d4393ef441e035f19771
Ghenadie Isacenco <isacenco@gmail.com>
2025-10-29 15:49:58 +0100

    Recipes View

---
+++ Avocados/View/RecipeCardView.swift
@@ -0,0 +1,89 @@
+//
+//  RecipeCardView.swift
+//  Avocados
+//
+//  Created by Ghenadie Isacenco on 29/10/2025.
+//
+
+import SwiftUI
+
+struct RecipeCardView: View {
+    // MARK: - PROPERTIES
+    
+    var recipe: Recipe
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            // MARK: - CARD IMAGE
+            Image(recipe.image)
+                .resizable()
+                .scaledToFit()
+                .overlay {
+                    HStack {
+                        Spacer()
+                        VStack {
+                            Image(systemName: "bookmark")
+                                .font(Font.title.weight(.light))
+                                .foregroundColor(.white)
+                                .imageScale(.small)
+                                .shadow(color: Color("ColorBlackTransparentLight"), radius: 2, x: 0, y: 0)
+                                .padding(.trailing, 20)
+                                .padding(.top, 22)
+                            Spacer()
+                        }
+                    }
+                }
+            
+            VStack(alignment: .leading, spacing: 12) {
+                // TITLE
+                Text(recipe.title)
+                    .font(.system(.title, design: .serif))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("ColorGreenMedium"))
+                    .lineLimit(1)
+                
+                // HEADLINE
+                Text(recipe.headline)
+                    .font(.system(.body, design: .serif))
+                    .foregroundColor(.gray)
+                    .italic()
+                
+                // RATES
+                HStack(alignment: .center, spacing: 5) {
+                    ForEach(1...(recipe.rating), id: \.self) { _ in
+                        Image(systemName: "star.fill")
+                            .font(.body)
+                            .foregroundColor(.yellow)
+                    }
+                }
+                
+                // COOKING
+                HStack(alignment: .center, spacing: 12) {
+                    HStack(alignment: .center, spacing: 2) {
+                        Image(systemName: "person.2")
+                        Text("Serves: \(recipe.serves)")
+                    }
+                    HStack(alignment: .center, spacing: 2) {
+                        Image(systemName: "clock")
+                        Text("Prep: \(recipe.preparation)")
+                    }
+                    HStack(alignment: .center, spacing: 2) {
+                        Image(systemName: "flame")
+                        Text("Cooking: \(recipe.cooking)")
+                    }
+                }
+                .font(.footnote)
+                .foregroundColor(.gray)
+            }
+            .padding()
+            .padding(.bottom, 12)
+        }
+        .background(.white)
+        .cornerRadius(12)
+        .shadow(color: Color("ColorBlackTransparentLight"), radius: 8, x: 0, y: 0)
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    RecipeCardView(recipe: recipesData[0])
+}
